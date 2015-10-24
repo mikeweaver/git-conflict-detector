fc5f939a2f98389e7bc22fd93b715b72036102c0
Mike Weaver <mweaver@invoca.com>
2015-10-23 22:46:16 -0700

    Add tests for Branch model and fix errors

--- models/branch.rb
+++ models/branch.rb
@@ -18,33 +18,33 @@ class Branch < ActiveRecord::Base
   has_many :users, as: :authors
   has_many :conflicts
 
-  def create_branch_from_git_data(branch_data)
-    branch = Branch.find_or_initialize_by(:name == branch_data.name)
-    branch.set!(
-        git_updated_at: branch_data.last_modified_date,
-        updated_at: Time.now) # force updated time
+  def self.create_branch_from_git_data(branch_data)
+    branch = Branch.find_or_initialize_by_name(branch_data.name)
+    branch.git_updated_at = branch_data.last_modified_date
+    branch.updated_at = Time.now # force updated time
+    branch.save
+    branch
   end
 
-  def create_conflict(branch_a, branch_b, checked_at_date)
+  def create_conflict(conflicting_branch, checked_at_date)
     # if conflict exists, then update the last_tested_date, set last_checked_date to nil
     # otherwise, insert conflict
-  end
-
-  def clear_conflict(branch_a, branch_b)
     x = 5
   end
 
-  scope :untested_branches, where(:git_updated_at > :git_tested_at).where(:git_tested_at == nil)
-
-  scope :branches_not_updated_since, lambda |checked_at_date|
-    where(:updated_at < checked_at_date)
+  def clear_conflict(formerly_conflicting_branch)
+    x = 5
   end
 
+  scope :untested_branches, where("git_tested_at IS ? OR git_updated_at > git_tested_at", nil)
+
+  scope :branches_not_updated_since, lambda { |checked_at_date| where("updated_at < ?", checked_at_date) }
+
   def to_s
-    @name
+    name
   end
 
   def =~(rhs)
-    @name =~ rhs
+    name =~ rhs
   end
 end
---
+++ spec/branch_spec.rb
@@ -0,0 +1,78 @@
+require_relative '../environment'
+require_relative '../models/branch.rb'
+require_relative '../lib/git.rb'
+require 'database_cleaner'
+
+RSpec.configure do |config|
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
+end
+
+describe 'Branch' do
+
+  it 'can create be constructed from git data' do
+    updated_at = DateTime.now
+    git_data = Git::Branch.new(
+        'mypath/mybranch',
+        updated_at,
+        'author@email.com')
+    branch = Branch.create_branch_from_git_data(git_data)
+
+    expect(branch.name).to eq('mypath/mybranch')
+    expect(branch.git_updated_at).to eq(updated_at)
+    expect(branch.git_tested_at).to be_nil
+    expect(branch.created_at).not_to be_nil
+    expect(branch.updated_at).not_to be_nil
+  end
+
+  context 'with two existing branches' do
+    before do
+      (0..1).each do |i|
+        git_data = Git::Branch.new(
+            "path/branch#{i}",
+            DateTime.now,
+            'author@email.com')
+        Branch.create_branch_from_git_data(git_data)
+      end
+    end
+
+    it 'returns branches_not_updated_since' do
+      expect(Branch.branches_not_updated_since(10.minutes.from_now).size).to eq(2)
+      expect(Branch.branches_not_updated_since(10.minutes.ago).size).to eq(0)
+    end
+
+    it 'returns untested_branches' do
+      expect(Branch.untested_branches.size).to eq(2)
+    end
+  end
+
+  context 'with an existing branch' do
+
+    before do
+      git_data = Git::Branch.new(
+          'mypath/mybranch',
+          DateTime.now,
+          'author@email.com')
+      @branch = Branch.create_branch_from_git_data(git_data)
+    end
+
+    it 'prints its name when stringified' do
+      expect(@branch.to_s).to eq('mypath/mybranch')
+    end
+
+    it 'can be compared with a regular expression' do
+      expect(@branch).to match(/mypath.*/)
+    end
+  end
+
+end
+
