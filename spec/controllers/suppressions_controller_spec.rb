6e9b40fad8fa889ebd5a9ca8ac5683380eb84015
Mike Weaver <mweaver@invoca.com>
2015-12-22 11:45:21 -0800

    Convert old style hash syntax

--- spec/controllers/suppressions_controller_spec.rb
+++ spec/controllers/suppressions_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe SuppressionsController, :type => :controller do
+describe SuppressionsController, type: :controller do
   render_views
 
   context "with a conflict" do
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe UsersController, :type => :controller do
+describe UsersController, type: :controller do
   render_views
 
   describe "GET unsubscribe" do
