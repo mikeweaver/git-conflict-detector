b7dc21097526edfa99bb3e97f469e6a398eed84c
Mike Weaver <mweaver@invoca.com>
2015-12-28 13:58:21 -0800

    Fix incorrect string substitution

--- app/mailers/conflicts_mailer.rb
+++ app/mailers/conflicts_mailer.rb
@@ -44,6 +44,6 @@ class ConflictsMailer < ActionMailer::Base
     @new_conflicts = new_conflicts.sort.sort
     @resolved_conflicts = resolved_conflicts.sort
     @existing_conflicts = existing_conflicts.sort
-    mail(to: user.email, bcc: GlobalSettings.bcc_emails, subject: 'Conflicts Detected in #{repository_name}', template_name: 'conflicts_email')
+    mail(to: user.email, bcc: GlobalSettings.bcc_emails, subject: "Conflicts Detected in #{repository_name}", template_name: 'conflicts_email')
   end
 end
