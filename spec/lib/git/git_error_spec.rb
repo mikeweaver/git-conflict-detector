74c88717ae2231e84a2eef0f0b9d51e796416b11
Mike Weaver <mweaver@invoca.com>
2016-02-12 22:04:17 -0800

    Fix line breaks in expected error messages

--- spec/lib/git/git_error_spec.rb
+++ spec/lib/git/git_error_spec.rb
@@ -4,14 +4,14 @@ describe 'Git::GitError' do
 
   it 'can be raised' do
     last_modified_date = Time.now
-    expect {raise Git::GitError.new('command', 200, 'error_message')}.to raise_exception(Git::GitError, 'Git command command failed with exit code 200. Message:\nerror_message')
+    expect {raise Git::GitError.new('command', 200, 'error_message')}.to raise_exception(Git::GitError, "Git command command failed with exit code 200. Message:\nerror_message")
   end
 
   it 'can be printed' do
     begin
       raise Git::GitError.new('command', 200, 'error_message')
     rescue Git::GitError => e
-      expect(e.message).to eq('Git command command failed with exit code 200. Message:\nerror_message')
+      expect(e.message).to eq("Git command command failed with exit code 200. Message:\nerror_message")
     end
   end
 end
