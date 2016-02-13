6c3e87ae89d026b9e64b9ecdb6ca4a8792380cda
Mike Weaver <mweaver@invoca.com>
2016-02-12 21:24:58 -0800

    Use attribute accessors rather than class members

--- lib/git/git_conflict.rb
+++ lib/git/git_conflict.rb
@@ -14,11 +14,11 @@ module Git
     end
 
     def ==(rhs)
-      @repository_name == rhs.repository_name && @branch_a == rhs.branch_a && @branch_b == rhs.branch_b && @conflicting_files == rhs.conflicting_files
+      repository_name == rhs.repository_name && branch_a == rhs.branch_a && branch_b == rhs.branch_b && conflicting_files == rhs.conflicting_files
     end
 
     def contains_branch(branch_name)
-      @branch_a == branch_name || @branch_b == branch_name
+      branch_a == branch_name || branch_b == branch_name
     end
   end
 end
