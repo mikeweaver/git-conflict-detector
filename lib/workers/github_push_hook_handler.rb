1bc8c84aa6de00db928523a80db105e7950c869a
Mike Weaver <mweaver@invoca.com>
2016-07-15 23:56:31 -0700

    Set status of GitHub SHAs

--- lib/workers/github_push_hook_handler.rb
+++ lib/workers/github_push_hook_handler.rb
@@ -1,10 +1,62 @@
 class GithubPushHookHandler
+  PENDING_QUEUE = 'pending'
+  PROCESSING_QUEUE = 'processing'
+  CONTEXT_NAME = 'JIRA Checker'
+  PENDING_DESCRIPTION = 'Branch is being examined'
+  SUCCESS_DESCRIPTION = 'Branch is OK'
+
   def initialize(push_hook_payload)
-    @payload = push_hook_payload
+    @payload = push_hook_payload.with_indifferent_access
   end
 
-  def handle!
+  def queue!
+    Rails.logger.info('Queueing request')
     Rails.logger.info(@payload)
+    set_status_for_repo(Github::Api::Status::STATE_PENDING, PENDING_DESCRIPTION)
+    process!
+    # TODO: Rescue exceptions
   end
-  handle_asynchronously :handle!
+  handle_asynchronously(:queue!, queue: PENDING_QUEUE)
+
+  def process!
+    Rails.logger.info('Processing request')
+    set_status_for_repo(Github::Api::Status::STATE_SUCCESS, SUCCESS_DESCRIPTION)
+    # TODO: Rescue exceptions
+  end
+  handle_asynchronously(:process!, queue: PROCESSING_QUEUE)
+
+  private
+
+  def set_status_for_repo(state, description)
+    api = Github::Api::Status.new(Rails.application.secrets.github_user_name,
+                                  Rails.application.secrets.github_password)
+    api.set_status(repository_owner_name,
+                   repository_name,
+                   sha,
+                   CONTEXT_NAME,
+                   state,
+                   description,
+                   'http://moreinfohere.com')
+  end
+
+  def sha
+    # TODO raise if SHA is all zeros
+    @payload[:after]
+  end
+
+  def repository
+    @payload[:repository] or raise 'Payload does not include repository'
+  end
+
+  def repository_name
+    repository[:name]
+  end
+
+  def repository_owner
+    repository[:owner] or raise 'Payload does not include repository owner'
+  end
+
+  def repository_owner_name
+    repository_owner[:name]
+  end
 end
--- spec/lib/workers/github_push_hook_handler_spec.rb
+++ spec/lib/workers/github_push_hook_handler_spec.rb
@@ -5,14 +5,45 @@ describe 'GithubPushHookHandler' do
     JSON.parse(File.read(Rails.root.join('spec/fixtures/github_push_payload.json')))
   end
 
+  def mock_status_request(state, description)
+    api = instance_double(Github::Api::Status)
+    expect(api).to receive(:set_status).with('OwnerName',
+                                             'reponame',
+                                             '6d8cc7db8021d3dbf90a4ebd378d2ecb97c2bc25',
+                                             GithubPushHookHandler::CONTEXT_NAME,
+                                             state,
+                                             description,
+                                             anything).and_return({})
+    expect(Github::Api::Status).to receive(:new).and_return(api)
+  end
+
   it 'can create be constructed from github push hook payload data' do
     handler = GithubPushHookHandler.new(load_payload)
     expect(handler).not_to be_nil
   end
 
-  it 'handle pushes using delayed jobs' do
-    GithubPushHookHandler.new(load_payload).handle!
+  it 'sets sha status when queued' do
+    mock_status_request(Github::Api::Status::STATE_PENDING, GithubPushHookHandler::PENDING_DESCRIPTION)
+    GithubPushHookHandler.new(load_payload).queue!
+
+    # a job should be queued
     expect(Delayed::Job.count).to eq(1)
+
+    # process the job
+    expect(Delayed::Worker.new.work_off(1)).to eq([1, 0])
+
+    # it should have queued another job
+    expect(Delayed::Job.count).to eq(1)
+  end
+
+  it 'sets sha status after processing' do
+    mock_status_request(Github::Api::Status::STATE_SUCCESS, GithubPushHookHandler::SUCCESS_DESCRIPTION)
+    GithubPushHookHandler.new(load_payload).process!
+
+    # a job should be queued
+    expect(Delayed::Job.count).to eq(1)
+
+    # process the job
     expect(Delayed::Worker.new.work_off).to eq([1, 0])
   end
 end
