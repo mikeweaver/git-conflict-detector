6efb481c6217d270c70c3fbda0e6f60ddcc89fc3
Mike Weaver <mweaver@invoca.com>
2016-07-16 23:45:30 -0700

    Create payload parsing class

---
+++ lib/github/api/push_hook_payload.rb
@@ -0,0 +1,66 @@
+module Github
+  module Api
+    class PushHookPayload
+      def initialize(payload)
+        @payload = payload.with_indifferent_access
+      end
+
+      def sha
+        head_commit[:id]
+      end
+
+      def message
+        head_commit[:message]
+      end
+
+      def branch_name
+        Git::GitBranch.name_from_ref(@payload[:ref])
+      end
+
+      def author_email
+        author[:email]
+      end
+
+      def author_name
+        author[:name]
+      end
+
+      def repository_name
+        repository[:name]
+      end
+
+      def repository_owner_name
+        repository_owner[:name]
+      end
+
+      def repository_path
+        "#{repository_owner_name}/#{repository[:name]}"
+      end
+
+      def git_branch_data
+        @git_branch_data ||= Git::GitBranch.new(repository_path,
+                                                branch_name,
+                                                DateTime::parse(head_commit[:timestamp]),
+                                                author_name,
+                                                author_email)
+      end
+
+      private
+      def head_commit
+        @payload[:head_commit] or raise 'Payload does not include head commit'
+      end
+
+      def author
+        head_commit[:author] or raise 'Payload does not include head commit author'
+      end
+
+      def repository
+        @payload[:repository] or raise 'Payload does not include repository'
+      end
+
+      def repository_owner
+        repository[:owner] or raise 'Payload does not include repository owner'
+      end
+    end
+  end
+end
--- lib/workers/github_push_hook_handler.rb
+++ lib/workers/github_push_hook_handler.rb
@@ -6,7 +6,8 @@ class GithubPushHookHandler
   SUCCESS_DESCRIPTION = 'Branch is OK'
 
   def initialize(push_hook_payload)
-    @payload = push_hook_payload.with_indifferent_access
+    @payload = Github::Api::PushHookPayload.new(push_hook_payload)
+
   end
 
   def queue!
@@ -14,14 +15,12 @@ class GithubPushHookHandler
     Rails.logger.info(@payload)
     set_status_for_repo(Github::Api::Status::STATE_PENDING, PENDING_DESCRIPTION)
     process!
-    # TODO: Rescue exceptions
   end
   handle_asynchronously(:queue!, queue: PENDING_QUEUE)
 
   def process!
     Rails.logger.info('Processing request')
     set_status_for_repo(Github::Api::Status::STATE_SUCCESS, SUCCESS_DESCRIPTION)
-    # TODO: Rescue exceptions
   end
   handle_asynchronously(:process!, queue: PROCESSING_QUEUE)
 
@@ -30,33 +29,13 @@ class GithubPushHookHandler
   def set_status_for_repo(state, description)
     api = Github::Api::Status.new(Rails.application.secrets.github_user_name,
                                   Rails.application.secrets.github_password)
-    api.set_status(repository_owner_name,
-                   repository_name,
-                   sha,
+    api.set_status(@payload.repository_owner_name,
+                   @payload.repository_name,
+                   @payload.sha,
                    CONTEXT_NAME,
                    state,
                    description,
                    'http://moreinfohere.com')
   end
 
-  def sha
-    # TODO raise if SHA is all zeros
-    @payload[:after]
-  end
-
-  def repository
-    @payload[:repository] or raise 'Payload does not include repository'
-  end
-
-  def repository_name
-    repository[:name]
-  end
-
-  def repository_owner
-    repository[:owner] or raise 'Payload does not include repository owner'
-  end
-
-  def repository_owner_name
-    repository_owner[:name]
-  end
 end
