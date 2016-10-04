00392705d03046d1ec83fd0aa8d7303d79f66ad3
Mike Weaver <mweaver@invoca.com>
2016-10-04 13:49:16 -0700

    Plumb basic error data into UI

--- app/controllers/jira_status_controller.rb
+++ app/controllers/jira_status_controller.rb
@@ -5,4 +5,20 @@ class JiraStatusController < ApplicationController
     flash[:alert] = 'The commit could not be found'
     redirect_to controller: 'errors', action: 'bad_request'
   end
+
+  def should_ignore_commit_errors(commit)
+    record = commit.commits_and_pushes.where(push: @push).first
+    record.errors.empty? || record.ignore_errors
+  end
+  helper_method :should_ignore_commit_errors
+
+  def github_url_for_commit(commit)
+    "https://github.com/#{@push.branch.repository.name}/commit/#{commit.sha}"
+  end
+  helper_method :github_url_for_commit
+
+  def jira_url_for_issue(jira_issue)
+    "#{GlobalSettings.jira.site}/browse/#{jira_issue.key}"
+  end
+  helper_method :jira_url_for_issue
 end
