0b00d9d18fa90827f45e4f980566e661612562af
Mike Weaver <mweaver@invoca.com>
2016-10-20 16:18:20 -0700

    Stop writing out Github public key, it is not needed

--- script/docker_entry_points/delayed_job.sh
+++ script/docker_entry_points/delayed_job.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 mkdir ~/.ssh
 echo -n $GITHUB_PRIVATE_KEY > ~/.ssh/id_rsa
-echo -n $GITHUB_PUBLIC_KEY > ~/.ssh/id_rsa.pub
 chmod 600 ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa.pub
 ssh-keyscan github.com > ~/.ssh/known_hosts
 eval "$(ssh-agent -s)"
 echo -n $JIRA_PRIVATE_KEY > ./jira_rsakey.pem
