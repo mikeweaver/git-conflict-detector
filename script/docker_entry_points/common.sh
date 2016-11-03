f9f3f5c4ba35dc6a1ebe9406799fa489ceecaded
Mike Weaver <mweaver@invoca.com>
2016-11-03 16:19:11 -0700

    Change the way we write out SSH keys to avoid munging new lines

--- script/docker_entry_points/common.sh
+++ script/docker_entry_points/common.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 if [ -n "$SETTINGS_FILE_CONTENT" ]; then
     mkdir -p ./data/config
-    ruby -ryaml -e 'puts ENV["SETTINGS_FILE_CONTENT"]' > ./data/config/settings.$RAILS_ENV.yml
+    ruby -e 'puts ENV["SETTINGS_FILE_CONTENT"]' > ./data/config/settings.$RAILS_ENV.yml
 fi
 if [ -n "$GITHUB_PRIVATE_KEY" ]; then
     mkdir -p ~/.ssh
-    echo -n $GITHUB_PRIVATE_KEY > ~/.ssh/id_rsa
+    ruby -e 'puts ENV["GITHUB_PRIVATE_KEY"]' > ~/.ssh/id_rsa
     chmod 600 ~/.ssh/id_rsa
     ssh-keyscan github.com > ~/.ssh/known_hosts 2>/dev/null
     eval "$(ssh-agent -s)"
 fi
 if [ -n "$JIRA_PRIVATE_KEY" ]; then
-    echo -n $JIRA_PRIVATE_KEY > $JIRA_PRIVATE_KEY_FILE
+    ruby -e 'puts ENV["JIRA_PRIVATE_KEY"]' > $JIRA_PRIVATE_KEY_FILE
 fi
